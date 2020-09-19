fdf51b49211866a575dfb320e8712b9e7b25255d
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-19 19:30:45 +0200

    added age and years range query

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,3 +1,4 @@
+const { min } = require('lodash');
 const Artist = require('../models/artist');
 
 /**
@@ -6,4 +7,18 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const getMinAge = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then((youngestArtists) => youngestArtists[0].age);
+
+  const getMaxAge = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then((eldestArtists) => eldestArtists[0].age);
+
+  return Promise.all([getMinAge, getMaxAge]).then((ages) => {
+    const [min, max] = ages;
+    return { min, max };
+  });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const getMinYearsActive = Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then((newestArtists) => newestArtists[0].yearsActive);
+
+  const getMaxYearsActive = Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then((eldestArtists) => eldestArtists[0].yearsActive);
+
+  return Promise.all([getMinYearsActive, getMaxYearsActive]).then(
+    (yearsActives) => {
+      const [min, max] = yearsActives;
+      return { min, max };
+    }
+  );
 };
