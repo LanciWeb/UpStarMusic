d84c3379bee3efe47b5176f8b22f7013aa612e4a
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-19 11:34:46 +0200

    created album model

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,13 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const AlbumSchema = new Schema({
+  title: { type: String, required: true },
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+});
+
+module.exports = AlbumSchema;
