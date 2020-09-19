5e183835e45794228474909057a44e1281d556b6
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-19 11:45:01 +0200

    added Artist Model

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,20 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const AlbumSchema = require('./album');
+
+const ArtistSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  Albums: [AlbumSchema],
+});
+
+const Artist = mongoose.model('Artist', ArtistSchema);
+
+module.exports = Artist;
